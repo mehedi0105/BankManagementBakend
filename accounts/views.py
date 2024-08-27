690c8226a0e5e998df82f8bceb24140f2997c550
mehedi0105 <mehedi01014@gmail.com>
2024-08-27 18:43:37 +0600

    add

--- accounts/views.py
+++ accounts/views.py
@@ -47,7 +47,7 @@ def activate(request, uid64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        return redirect("http://127.0.0.1:8000/accounts/register/")
+        return redirect("http://127.0.0.1:5500/Frontend/login.html")
     else:
         return HttpResponse("Activation link is invalid!", status=status.HTTP_400_BAD_REQUEST)
     
